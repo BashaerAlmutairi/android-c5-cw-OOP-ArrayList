a41679ae07479bdfe1481a94445b4fc3d8d51cb1
BashaerAlmutairi <bashaer.n.nasser@gmail.com>
2022-03-16 20:32:01 +0300

    Done

--- Android School/app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ Android School/app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -9,10 +9,8 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    /**
-     *  عرفوا مصفوفة بإسم students
-     *  define an arraylist named students
-     */
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.studentListView);
 
-        /**
-         *  إكتبوا الكود بعد هذا السطر
-         *  ضيفوا عناصر من الكلاس
-         *  add objects from class Student
-         */
+
+
+        Student s1 = new Student ("Yousef", 14 , 12 , R.drawable.boy1);
+        Student s2 = new Student ("Salman", 13 , 10 , R.drawable.boy1);
+        Student s3 = new Student ("Majed", 15 , 11 , R.drawable.boy1);
+
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(s1); // add item to ArrayList
+        students.add(s2); // add item to ArrayList
+        students.add(s3); // add item to ArrayList
+
 
 
         StudentAdapter studentAdapter = new StudentAdapter(this,0,students);
